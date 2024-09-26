ff0328427d533e8373214a4e8a26053a1dfa0f63
alexten1st <aaten1994@mail.ru>
2024-09-26 13:41:04 +0300

    create tests

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -5,21 +5,21 @@ describe('PostsService', () => {
   const post: Omit<Post, 'id' | 'date'> = {
     text: 'Mocked post',
   };
-  let postWithIdAndDate: Post;
 
   beforeEach(async () => {
     postsService = new PostsService();
 
-    postWithIdAndDate = postsService.create(post)
+    postsService.create(post)
   });
 
   it('should add a new post', () => {
-    expect(postsService.find(postWithIdAndDate.id)).toBe(postWithIdAndDate)
+    expect(postsService.find('1')!.text).toEqual(post.text)
   });
 
   it('should find a post', () => {
     const posts = [{text: 'Mocked post1',}, {text: 'Mocked post2',}, {text: 'Mocked post3',}];
     posts.forEach((el)=>postsService.create(el))
-    expect(postsService.find(postWithIdAndDate.id)).toEqual(postWithIdAndDate)
+
+    expect(postsService.find('1')!.text).toEqual(post.text)
   });
 });
